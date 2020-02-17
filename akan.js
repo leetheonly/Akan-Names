6ee17b73f43b8fbea4166fd15383dedf9ffa4dc3
leetheonly <lawrenceireri201@gmail.com>
2020-02-16 22:04:43 -0500

    Add modified akan.html akan.js

--- akan.js
+++ akan.js
@@ -1,9 +1,14 @@
-var male = [Kwasi,Kwadwo,Kwabena,KwakuYaw,Kofi,Kwame]
+function output()
+var day = document.getElementById("day").value;
+var dd = parseInt(day);
+var month = document.getElementById("month").value;
+var mm = parseInt (month);
+var male = [Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame]
 var female = [Akosua,Adwo,Abenaa,Akua,Yaa,Afua,Ama]
+
 var day = [Sunday,Monday,Teusday,Wednesday,Thursday,Friday,Saturday]
-prompt("what is your date of birth");//I'm going to type date>0||date<31
-if (date<0||date>31) { 
-    
-} else {
-    
-}
+var gender = parseInt("male")
+
+
+
+
