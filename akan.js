55224b6d2fe8e61d1db75e9064031fb190bbd813
leetheonly <lawrenceireri201@gmail.com>
2020-02-16 20:42:22 -0500

    add modified akan.html

--- akan.js
+++ akan.js
@@ -1,1 +1,9 @@
-var 
+var male = [Kwasi,Kwadwo,Kwabena,KwakuYaw,Kofi,Kwame]
+var female = [Akosua,Adwo,Abenaa,Akua,Yaa,Afua,Ama]
+var day = [Sunday,Monday,Teusday,Wednesday,Thursday,Friday,Saturday]
+prompt("what is your date of birth");//I'm going to type date>0||date<31
+if (date<0||date>31) { 
+    
+} else {
+    
+}
