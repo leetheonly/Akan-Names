1d119012fca2c4f560c03c9c5d0afc73f7a6636b
leetheonly <lawrenceireri201@gmail.com>
2020-02-18 15:44:18 -0500

    add modified akan.js index.html

--- akan.js
+++ akan.js
@@ -6,7 +6,6 @@ function akanName() {
     var maleGender = document.getElementById("maleGender");
     var date = new Date(yy + "-" + mm + "-" + dd);
     var dayBorn = date.getDay();
-   
     var maleNames = [ "Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
